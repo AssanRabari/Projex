516453882b8c5fbd22b959f67698e3f2e91b10ae
Assan Rabari <assandesai@gmail.com>
2025-05-04 09:12:59 +0530

    Sidebar, Navbar design and updated metadata

--- client/src/app/DashBoardWrapper.tsx
+++ client/src/app/DashBoardWrapper.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
 
-const DashBoardWrapper = ({children}: {children:React.ReactNode}) => {
+const DashBoardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        {/* Sidebar */}
-        Sidebar
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
-            {/* Navbar */}
-            <Navbar />
-            {children}
-        </main>
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        {/* Navbar */}
+        <Navbar />
+        {children}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default DashBoardWrapper
+export default DashBoardWrapper;
